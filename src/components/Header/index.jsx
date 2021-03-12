0693d736b22335c3802ee928ccff82d86a88a758
Cassiano Thurow <cassiano.thurow@gmail.com>
2021-03-12 11:07:58 -0300

    :tada: Begin develop

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function Header() {
+  return (
+    <View>
+      <Text>Header</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
---
+++ src/pages/Search/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export default function Search() {
+  return (
+    <View>
+      <Text>Pagina de Busca</Text>
+    </View>
+  );
+}
